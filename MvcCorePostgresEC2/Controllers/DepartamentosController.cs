6a9c544c69bd982c203fd12f962ee9874b018a71
Alberto Rodríguez-Rey Prieto <alberto.rodriguez-rey@tajamar365.com>
2026-05-13 13:13:46 +0200

    Add project files.

---
+++ MvcCorePostgresEC2/Controllers/DepartamentosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCorePostgresEC2.Repositories;
+
+namespace MvcCorePostgresEC2.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private RepositoryDepartamentos repo;
+
+        public DepartamentosController(RepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var departamentos = await this.repo.GetDepartamentosAsync();
+            return View(departamentos);
+        }
+        //DETAILS
+        public async Task<IActionResult> Details(int id)
+        {
+            var dept = await this.repo.FindDepartamentoAsync(id);
+            return View(dept);
+        }
+        //CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(int idDepartamento, string nombre, string localidad)
+        {
+            await this.repo.CreateDepartamento(idDepartamento, nombre, localidad);
+            return RedirectToAction("Index");
+        }
+    }
+}
